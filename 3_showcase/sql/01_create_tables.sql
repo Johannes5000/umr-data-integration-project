788d3fc52dcab04360ffbecdb9fc6d82743022ff
ClemensWissel <wisselclemens@gmail.com>
2022-06-21 23:23:06 +0200

    upd

--- 3_showcase/sql/01_create_tables.sql
+++ 3_showcase/sql/01_create_tables.sql
@@ -1,4 +1,4 @@
-drop table if exists showcase cascade;
+drop table if exists showcase;
 create table showcase (
 		ingredient_recipe_id Bigint,
 		ingredient_name text,
@@ -19,11 +19,12 @@ create table showcase (
 		first_token_similarity numeric,
 		unit text,
 		factor numeric,
-		ingredient_base_amount numeric
+		ingredient_base_amount numeric,
+		product_base_amount numeric
 );
 
 with liquids as (
-	select *, ingredient_amount * factor as ingredient_base_amount
+	select *, ingredient_amount * factor as ingredient_base_amount, product_amount * factor as product_base_amount
 	from view_cleaned_data join convert_volume_to_l on view_cleaned_data.ingredient_unit = convert_volume_to_l.unit
 	where ingredient_unit in (
 		select unit 
@@ -33,9 +34,9 @@ with liquids as (
 insert into showcase (select * from liquids);
 
 with mass as (
-	select *, ingredient_amount * factor as ingredient_base_amount 
-	from view_cleaned_data join convert_mass_to_kg on view_cleaned_data.ingredient_unit = convert_mass_to_kg.unit
-	where ingredient_unit in (
+	select *, ingredient_amount * factor as ingredient_base_amount, product_amount * factor as product_base_amount
+	from view_cleaned_data join convert_mass_to_kg on view_cleaned_data.product_base_unit = convert_mass_to_kg.unit
+	where product_base_unit in (
 		select unit 
 		from convert_mass_to_kg
 	)
